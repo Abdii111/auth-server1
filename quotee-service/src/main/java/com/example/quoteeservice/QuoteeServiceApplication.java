d7b1902579b4e0990b73be65d90636cde0a93d3b
Abdifatah Abdulkadir <abdifatah.abdulkadir@iths.se>
2025-06-03 22:07:24 +0200

    final

---
+++ quotee-service/src/main/java/com/example/quoteeservice/QuoteeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.quoteeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteeServiceApplication.class, args);
+    }
+
+}
---
+++ quotee-service/src/test/java/com/example/quoteeservice/QuoteeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quoteeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
